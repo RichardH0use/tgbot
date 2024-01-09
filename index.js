00fb388bd200a44abe4271339625219af2056d15
RichardH0use <125386677+RichardH0use@users.noreply.github.com>
2024-01-09 14:21:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
 const token = '6968485063:AAGBH5VvCG-_5sFsrTlOwkZui_3K1Or2epo'
-global.bot = new TelegramBot(token, { polling: true })
+global.bot = new TelegramBot(token, { polling: true, webhook.port: process.env.PORT })
 bot.on('message', (msg) => {
     console.log(msg)
     if (msg.chat.id == '1080302297' && msg.chat.username == 'roberth0') {
